c544db92fa785b171c8bc5a0069d52c9087d1722
darkstar112358 <abhishekkadiyan@gmail.com>
2013-07-17 12:38:31 +0530

    readme update, directories renamed 2

---
+++ RichChatLibrary/src/com/example/EChatLibrary/ThumbnailInfo.java
@@ -0,0 +1,13 @@
+package com.example.EChatLibrary;
+
+// for holding the thumbnail information
+
+public class ThumbnailInfo {
+
+    public String enteredURL;
+
+    public ThumbnailInfo(String _url) {
+        enteredURL = _url;
+    }
+
+}
